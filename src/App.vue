842937a71a0c853f618f5509aeb8c3907d42ddfd
takahashiyb <takahashiyb89@gmail.com>
2026-03-26 06:29:33 +0800

    Set up supabase

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,57 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { supabase } from '@/lib/supabaseClient.ts'
+import EngagementCard from './components/EngagementCard.vue'
+
+import type { Comments } from '@/types/comments'
+import type { Users } from '@/types/users'
+
+const users = ref<Users[]>([])
+
+async function getUsers() {
+  const { data, error } = await supabase.from('users').select('*')
+  users.value = data as Users[]
+
+  if (error) {
+    console.log(error)
+  }
+}
+
+const comments = ref<Comments[]>([])
+
+async function getComments() {
+  const { data, error } = await supabase.from('comments').select('*').eq('type', 'comment')
+  comments.value = data as Comments[]
+
+  if (error) {
+    console.log(error)
+  }
+}
+
+const replies = ref<Comments[]>([])
+
+async function getReplies() {
+  const { data, error } = await supabase.from('comments').select('*')
+  replies.value = data as Comments[]
+
+  if (error) {
+    console.log(error)
+  }
+}
+
+onMounted(() => {
+  getUsers()
+  getComments()
+  getReplies()
+})
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <ul>
+    <li v-for="comment in comments">
+      <EngagementCard :details="comment" :user="users.find((i) => i.id === comment.user_id)" />
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>

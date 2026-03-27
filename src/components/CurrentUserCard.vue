d65a81b42ebbc68c7db3610f4964822f7f7f962f
takahashiyb <takahashiyb89@gmail.com>
2026-03-27 14:50:27 +0800

    Create function to insert to supabase

--- src/components/CurrentUserCard.vue
+++ src/components/CurrentUserCard.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { supabase } from '@/lib/supabaseClient.ts'
 import { useDataStore } from '@/stores/data'
 import { ref, watchEffect } from 'vue'
 
@@ -12,9 +13,36 @@ const props = defineProps<{
 
 const emits = defineEmits(['confirmReply'])
 
+const textarea = ref<string | null>(null)
+
 watchEffect(async () => {
   avatar.value = await dataStore.fetchWithRetry(dataStore.currentUser)
 })
+
+async function insertData(type: 'reply' | 'send') {
+  if (!textarea.value) return
+
+  const { data, error } = await supabase
+    .from('comments')
+    .insert([
+      {
+        content: textarea.value,
+        type: props.type,
+        parent_comment: props.type === 'reply' ? dataStore.actionFocus : null,
+        user_id: dataStore.currentUser?.id,
+      },
+    ])
+    .select()
+
+  if (type === 'reply') {
+    emits('confirmReply')
+  }
+
+  await dataStore.getComments()
+
+  textarea.value = ''
+  console.log(dataStore.comments)
+}
 </script>
 <template>
   <div class="card card__current-user">
@@ -22,23 +50,32 @@ watchEffect(async () => {
 
     <textarea
       class="textarea font-2-r color-grey-500"
-      name="new-comment"
+      data-function="textarea-user"
+      :name="`new-${props.type}`"
       :placeholder="`Add a ${props.type}...`"
+      v-model="textarea"
       >{{
         `${props.type === 'comment' ? '' : '@' + dataStore.users.find((i) => i.id === dataStore.comments.find((i) => i.id === dataStore.actionFocus)?.user_id)?.username + ' '}`
       }}</textarea
     >
 
-    <button class="button--purple" type="button" v-if="props.type === 'comment'">
+    <button
+      class="button--purple"
+      data-function="send"
+      type="button"
+      @click="insertData('send')"
+      v-if="props.type === 'comment'"
+    >
       <span class="sr-only">Click this button to submit your comment</span>
       <span class="font-2-m">SEND</span>
     </button>
 
     <button
       class="button--purple"
+      data-function="confirm-reply"
       type="button"
       v-if="props.type === 'reply'"
-      @click="$emit('confirmReply')"
+      @click="insertData('reply')"
     >
       <span class="sr-only">Click this button to submit your reply</span>
       <span class="font-2-m">REPLY</span>
